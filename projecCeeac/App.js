d9a35759f2fc4c3f1668307513d85fa68637a4e0
Caio Ferreira <fcaio127@uni9.edu.br>
2022-06-26 14:36:22 -0300

    e mais dificil do que eu pensava

--- projecCeeac/App.js
+++ projecCeeac/App.js
@@ -1,60 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
-const UselessTextInput = () => {
-  const [text, onChangeText] = React.useState(null);
-  const [number, onChangeNumber] = React.useState(null);
-
+export default function App() {
   return (
-      <View style={styles.centered}>
-      <Text 
-        style={styles.container}>
-        Bem vindo!
-      </Text>
-     <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        placeholder="Insira seu email"
-        keyboardType="email"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="Insira a senha"
-        keyboardType="numeric"
-      />
-    </SafeAreaView>
-    <Button
-      color= '#000'
-      title='Entrar'
-      onPress={() => Alert.alert('Botao funcionando')}
-      />
+    <View style={styles.card}>
+      <Text styles={styles.title}>bem vindo</Text>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-    container: {
-      padding: 12,
-      alignContent: 'center',
-      justifyContent: 'center',
+  card: {
+   flex: 1,
+   backgroundColor: '#2A2A2A',
+   justifyContent: 'center',
+   alignItems: 'center',
   },
-  input: {
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-    borderRadius: 4
-  },
-  centered: {
-    flex: 1,
-    alignContent: 'center',
-    justifyContent: 'center'
-    
-  },
+  title: { 
+    color: '#fff',
+    backgroundColor: '#fff'
+  }
 });
-
-export default UselessTextInput;
