2f8a23a725eb65113172334c61121ab211b88597
Caio Ferreira <fcaio127@uni9.edu.br>
2022-06-26 13:06:03 -0300

    input formado

--- projecCeeac/App.js
+++ projecCeeac/App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Button, Alert } from 'react-native';
 
 const UselessTextInput = () => {
   const [text, onChangeText] = React.useState(null);
   const [number, onChangeNumber] = React.useState(null);
 
   return (
-    <View style={styles.centered}>
-    <SafeAreaView>
+      <View style={styles.centered}>
+      <Text 
+        style={styles.container}>
+        Bem vindo!
+      </Text>
+     <SafeAreaView>
       <TextInput
         style={styles.input}
         onChangeText={onChangeText}
@@ -24,23 +28,33 @@ const UselessTextInput = () => {
         keyboardType="numeric"
       />
     </SafeAreaView>
+    <Button
+      color= '#000'
+      title='Entrar'
+      onPress={() => Alert.alert('Botao funcionando')}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+    container: {
+      padding: 12,
+      alignContent: 'center',
+      justifyContent: 'center',
+  },
   input: {
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    borderRadius: 4,
+    borderRadius: 4
   },
   centered: {
     flex: 1,
     alignContent: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
     
-  }
+  },
 });
 
 export default UselessTextInput;
