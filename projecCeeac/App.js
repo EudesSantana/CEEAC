7746cca5f5f87eadcf7e91a191803383cbdc80bf
Caio Ferreira <fcaio127@uni9.edu.br>
2022-06-29 20:31:13 -0300

    pastas ok

--- projecCeeac/App.js
+++ projecCeeac/App.js
@@ -1,50 +1,21 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, TextInput, View, Alert, Button } from "react-native";
+import { statusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View} from 'react-native';
 
-const UselessTextInput = () => {
-  const [text, onChangeText] = React.useState(null);
-  const [number, onChangeNumber] = React.useState(null);
-
+export default function App() {
   return (
-    <View style={styles.container}>
-      
-    <SafeAreaView>
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-        placeholder="Insira seu email"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="Insira sua senha"
-        keyboardType="numeric"
-      />
-      <Button
-        title="Entrar"
-        onPress={() => Alert.alert('Button Ok')}
-        color="2A2A2A"
-      />
-    </SafeAreaView>
+    <View styles={styles.container}>
+
+
     </View>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-    container: {
+const styles = styleSheet.create({
+  container: {
     flex: 1,
-    justifyContent: 'left',
+    backgroundColor: '###',
     alignItems: 'center',
-    padding: 10,
+    justifyContent: 'center',
   },
 });
-
-export default UselessTextInput;
