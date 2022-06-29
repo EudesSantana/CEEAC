29e074a94e1f1bd1b93412634036ab28d0fc7337
Caio Ferreira <fcaio127@uni9.edu.br>
2022-06-29 20:49:19 -0300

    adc fundo

--- projecCeeac/App.js
+++ projecCeeac/App.js
@@ -1,11 +1,13 @@
 import { statusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View styles={styles.container}>
       <statusBar hidden />
+
+      <Image source={require('./assests/img.jpg')}/>
 
     </View>
   );
