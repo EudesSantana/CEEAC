b81324a1db9fe28bb0ba74b4126990c9546faca0
Caio Ferreira <fcaio127@uni9.edu.br>
2022-07-09 18:10:49 -0300

    ...

--- projecCeeac/App.js
+++ projecCeeac/App.js
@@ -33,7 +33,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2a2a2a',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
